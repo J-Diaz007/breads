e1959fc7094de43762b3b37d19b15b3bc219e14b
JDiaz <jdiaz_007@outlook.com>
2023-08-22 19:56:37 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,7 +14,14 @@ breads.get('/', (req, res) => {
 
 module.exports = breads
 
-//Show
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-})
+    if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+      })
+    } else {
+      res.render('Not Found')
+    }
+  })
+  
