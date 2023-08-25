d9ba19698c49202618175675825d575de0b0fb76
JDiaz <jdiaz_007@outlook.com>
2023-08-24 19:49:34 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -48,9 +48,29 @@ breads.post('/', (req, res) => {
   })
   
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
+breads.delete('/:arrayIndex', (req, res) => {
+  Bread.splice(req.params.arrayIndex, 1)
   res.status(303).redirect('/breads')
+})
+
+// EDIT
+breads.get('/:arrayIndex/edit', (req, res) => {
+  res.render('Edit', {
+    bread: Bread[req.params.arrayIndex],
+    index: req.params.arrayIndex
+  })
+})
+
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
